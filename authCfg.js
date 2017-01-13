be8a5589fbf63b3bd44e63ec9529e917de57841c
arthurbic <arthurbik@gmail.com>
2017-01-12 22:18:19 -0200

    Add files via upload

---
+++ authCfg.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/return' 
+    },
+
+//no developers.facebook.com
+    //tem que criar um novo produto, login com facebook
+
+
+    'googleAuth' : {
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : 'http://127.0.0.1:3000/auth/google/return'
+    }
+
+};
